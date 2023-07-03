b5eae294b6509976f5cb22f3fa48415315519219
christiannaira <christianmnaira@gmail.com>
2023-07-04 01:48:11 +0630

    designed the useraccount section

--- client/src/pages/UserAccount.jsx
+++ client/src/pages/UserAccount.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import UserProfile from '../assets/profile.png';
 
 function UserAccount() {
 
@@ -42,12 +43,64 @@ function UserAccount() {
                 <div className="useraccount-content">
 
                     <div className="useraccount-content-main">
-                        {
-                            (user === null) ? "" : <h1>Hi,{user.name}!</h1>
-                        }
-                        <button onClick={handleLogout} className="btn btn-dark mt-3">
-                            Logout
-                        </button>
+
+                    </div>
+
+                    <div className="user-activities container   ">
+
+                        <div className="row">
+
+                            <div className="col-md-12 user">
+
+                                <div className="useraccount-text">
+
+                                    <div className="user-profile d-flex align-items-center justify-content-start">
+
+                                        <div className="user-img">
+                                            <img src={UserProfile} alt="" width={'100px'} />
+                                        </div>
+
+                                        {
+                                            (user === null) ? "" : <h1>{user.name} <i class='bx bxs-check-circle'></i></h1>
+                                        }
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+
+                            <div className="cold-md-12">
+
+                                <h3>Contact</h3>
+                                <p>No registered services, you can add some on the settings page.</p>
+
+                            </div>
+
+                            <div className="cold-md-12">
+
+                                <h3>Biography</h3>
+                                <p>{(user === null) ? "" : user.name} hasn't added a bio</p>
+
+                            </div>
+
+
+                            <div className="cold-md-12">
+
+                                <h3>Links</h3>
+                                <p>{(user === null) ? "" : user.name} hasn't added any links</p>
+
+                            </div>
+
+
+                            <div className="profile-btn">
+                                <button onClick={handleLogout} className="btn btn-dark mt-3">
+                                    Logout
+                                </button>
+                            </div>
+
+                        </div>
+
                     </div>
 
                 </div>
